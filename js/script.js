4ddf801e05e3bc531254ad532ea90c6c27d230b4
NoonShin <nooshin.shahidzadeh@gmail.com>
2020-04-13 02:20:05 +0200

    Added ER

--- js/script.js
+++ js/script.js
@@ -4,3 +4,15 @@ $(function(){
 	$('.thumbs a').touchTouch();
 
 });
+
+function changeImage() {
+
+        if (document.getElementById("imgClickAndChange").getAttribute("src") == "images/ER_drawing.jpeg")
+        {
+            document.getElementById("imgClickAndChange").src = "images/ER.png";
+        }
+        else
+        {
+            document.getElementById("imgClickAndChange").src = "images/ER_drawing.jpeg";
+        }
+    }
