141b3c15094206e527bfec9c43da06abba2868e4
NoonShin <nooshin.shahidzadeh@gmail.com>
2020-04-15 19:51:26 +0200

    bug fixes

--- js/script.js
+++ js/script.js
@@ -11,8 +11,16 @@ function changeImage() {
         {
             document.getElementById("imgClickAndChange").src = "images/ER.png";
         }
-        else
+        else if (document.getElementById("imgClickAndChange").getAttribute("src") == "images/ER.png")
         {
             document.getElementById("imgClickAndChange").src = "images/ER_drawing.jpeg";
+        }
+				else if (document.getElementById("imgClickAndChange").getAttribute("src") == "images/Early_Theoretical.png")
+        {
+            document.getElementById("imgClickAndChange").src = "images/Final_Theoretical.png";
+        }
+				else if (document.getElementById("imgClickAndChange").getAttribute("src") == "images/Final_Theoretical.png")
+        {
+            document.getElementById("imgClickAndChange").src = "images/Early_Theoretical.png";
         }
     }
